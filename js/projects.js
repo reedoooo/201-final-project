f0ae8cb106001d3dc660d038a2ea1592e70f14ee
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-01-31 18:04:59 -0800

    trying to get files to display in iframes

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,16 @@
+'use strict'
+
+// function File (name, size, type, webkitRelativePath) {
+//     this.name = name;
+//     this.size = size;
+//     this.type = type;
+//     this.webkitRelativePath = webkitRelativePath;
+// }
+
+// function Folder () {
+//     this.files = {};
+// }
+
 function Project(title, path, files) {
     this.title = title;
     this.path = path;
@@ -6,17 +19,36 @@ function Project(title, path, files) {
     this.fileSize = 0;
 }
 
+Project.prototype.convertFilesObjectToArray = function () {
+    // console.log(this.files)
+    // let newFiles = [];
+    // for (let i = 0; i < this.files.length; i++) {
+    //     // console.log(this.files[i].name);
+    //     let newFile = new File (this.files[i].name, this.files[i].size, this.files[i].type, this.files[i].webkitRelativePath);
+    //     newFiles.push(newFile);
+    // }
+    // this.files = newFiles;
+    // console.log(this.files);
+}
+
+
 function addProjectToState(event) {
 event.preventDefault();
+
 let title = event.target.title.value;
 let path = event.target.folderSRC.value;
-let files = event.target.folderSRC.files; 
-console.log(event.target.folderSRC.files);
+let files = event.target.folderSRC.files;
+
 let project = new Project(title, path, files);
+
+// project.convertFilesObjectToArray();
+
 state.projects.push(project);
-console.log(state.projects);
-console.log(localStorage.portfolioState);
+
 saveAllToLocalStorage();
+clearProjectsList();
+populateProjectsList();
+
 }
 
 function clearProjectsState(){
@@ -27,22 +59,32 @@ function handleDisplayCode(event){
     let codeEl = document.getElementById('code');
     let webPageEl = document.getElementById('webPage');
     // codeEl.src = state.projects[event.target.id].path;
-    console.log(state.projects[event.target.id].files);
+    // console.log(state.projects[event.target.id].files);
+
+    // codeEl.src = state.projects[event.target.id].files[1].name;
 }
 
 function populateProjectsList(){
     retrieveAllFromLocalStorage();
-    console.log(state.projects);
+
     for (let i = 0; i < state.projects.length; i++) {
         let folderRowEl = document.getElementById('folderRow');
         let folderButtonEl = document.createElement('button');
+
         folderButtonEl.setAttribute('class', 'folder');
         folderButtonEl.setAttribute('id', i);
         folderButtonEl.innerText = state.projects[i].title;
-        // document.querySelector(`#folder${i}:before`).style.content = state.projects[i].title;
-        console.log(folderButtonEl);
         folderButtonEl.addEventListener('click', handleDisplayCode);
+
         folderRowEl.appendChild(folderButtonEl);
+    }
+}
+
+function clearProjectsList() {
+    const folderRowEl = document.getElementById('folderRow');
+
+    for (let i = 0; folderRowEl.childNodes.length > 0; i++) {
+        folderRowEl.removeChild(folderRowEl.lastChild);
     }
 }
 
