b4050a665afd33337721682f7c8f76c9f2bcee29
reedoooo <readvogt@gmail.com>
2023-01-31 20:09:20 -0800

    work on color change function

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 'use strict';
 
-let formSubmit = document.getElementById('submit-button');
+let formSubmit = document.getElementById('login-submit');
+let confirmInfo = document.getElementById('confirm-button');
+// let radioButton = document.getElementsByClassName('radio');
 
 let logIn = function (event) {
   event.preventDefault();
@@ -8,31 +10,60 @@ let logIn = function (event) {
   let inputLightTheme = event.target.light.checked; //.light.checked = true or false
   let inputDarkTheme = event.target.dark.checked;
 
-  if (inputLightTheme) {
-    homePageLight();
-  } else if (inputDarkTheme) {
-    homePageDark();
+  if (inputDarkTheme) {
+    setTheme('dark');
+  } else if (inputLightTheme) {
+    setTheme('light');
   }
-
 };
 
-formSubmit.addEventListener('submit', logIn);
 
-let homePageLight = function () {
-  console.log('light theme');
-  document.getElementById('body-div').style.backgroundColor='#ffaec0';
-  document.getElementById('body-div').style.color='black';
-  document.getElementById('legend-div').style.backgroundColor='#f789a1';
-  document.getElementById('login-form').style.backgroundColor='#f06c89';
-  document.getElementById('login-submit').style.backgroundColor='#FFE9F3';
+
+formSubmit.addEventListener('submit', ()=> {
+  logIn();
+  let selectedTheme = document.querySelector('input[type="radio"][name="form"]').value;
+  setTheme(selectedTheme);
+  console.log(selectedTheme);
+});
+
+let darkArray = ['#453F3C','C1EDCC','#534e4c','#797270','#C1EDCC'];
+let lightArray = ['#ffaec0','black','#f789a1','#f06c89','#FFE9F3'];
+
+// localStorage.setItem('elementArray', JSON.stringify(elementArray));
+// localStorage.setItem('lightTheme', JSON.stringify(lightArray));
+// localStorage.setItem('darkTheme', JSON.stringify(darkArray));
+
+
+function setTheme(theme) {
+  if (theme === 'dark') {
+    document.getElementById('body-div').style.backgroundColor=darkArray[0];
+    document.getElementById('body-div').style.color=darkArray[1];
+    document.getElementById('legend-div').style.backgroundColor=darkArray[2];
+    document.getElementById('login-form').style.backgroundColor=darkArray[3];
+    document.getElementById('login-submit').style.backgroundColor=darkArray[4];
+  } else {
+    document.getElementById('body-div').style.backgroundColor=lightArray[0];
+    document.getElementById('body-div').style.color=lightArray[1];
+    document.getElementById('legend-div').style.backgroundColor=lightArray[2];
+    document.getElementById('login-form').style.backgroundColor=lightArray[3];
+    document.getElementById('login-submit').style.backgroundColor=lightArray[4];
+  }
+  localStorage.setItem('theme', theme);
+}
+
+// On page load, check if a theme has been saved in local storage
+window.addEventListener('load', function() {
+  let savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    setTheme(savedTheme);
+  }
+});
+
+confirmInfo.addEventListener('click', changePage);
+
+let changePage = function () {
+  window.location.href = ('index.html');
 };
 
-let homePageDark = function () {
-  console.log('dark theme');
-  document.getElementById('body-div').style.backgroundColor='#453F3C';
-  document.getElementById('body-div').style.color='C1EDCC';
-  document.getElementById('legend-div').style.backgroundColor='#534e4c';
-  document.getElementById('login-form').style.backgroundColor='#797270';
-  document.getElementById('login-submit').style.backgroundColor='#C1EDCC';
-};
+module.exports = setTheme;
 
