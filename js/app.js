1f24c06448688409aabf46b8e022860e287275a4
reedoooo <readvogt@gmail.com>
2023-02-01 20:53:06 -0800

    changes to app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,37 @@
 'use strict';
 
+import setTheme from './login.js';
+
+window.addEventListener('load', function () {
+  let savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    setTheme(savedTheme);
+  }
+});
+
 let state = {
-    projects: [],
-    notes: [
-        {
-            title: 'CSS Notes',
-            text: 'These are my notes about CSS. CSS is frickin sweet.'
-        },
-        {
-            title: 'HTML Notes',
-            text: 'These are my really well formatted HTML notes. Semantic tags are cool.'
-        },
-        {
-            title: 'JavaScript Notes',
-            text: 'JavaScript is my favorite language! Constructors are fun!'
-        }
-    ],
+  projects: [],
+  notes: [
+    {
+      title: 'CSS Notes',
+      text: 'These are my notes about CSS. CSS is frickin sweet.'
+    },
+    {
+      title: 'HTML Notes',
+      text: 'These are my really well formatted HTML notes. Semantic tags are cool.'
+    },
+    {
+      title: 'JavaScript Notes',
+      text: 'JavaScript is my favorite language! Constructors are fun!'
+    }
+  ],
 }
 
-function saveAllToLocalStorage () {
-    localStorage.setItem('portfolioState', JSON.stringify(state));
+function saveAllToLocalStorage() {
+  localStorage.setItem('portfolioState', JSON.stringify(state));
 }
 
-function retrieveAllFromLocalStorage () {
-    state = JSON.parse(localStorage.getItem('portfolioState'));
+function retrieveAllFromLocalStorage() {
+  state = JSON.parse(localStorage.getItem('portfolioState'));
 }
+
